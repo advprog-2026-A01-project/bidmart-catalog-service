c1c1bc90539fa4c5f91843caa10ff1935c3c21f8
sahilaathia <athiasahila@gmail.com>
2026-05-04 08:59:32 +0700

    init

---
+++ src/main/java/id/ac/ui/cs/advprog/bidmartcatalogservice/BidmartCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmartcatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidmartCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidmartCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/bidmartcatalogservice/BidmartCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmartcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidmartCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
